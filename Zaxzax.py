533db08bf20e9d13b180efff7f335531789c07c5
zaxizax <franz.mlynek@gmx.at>
2022-06-10 11:41:05 +0200

    new version 1.0

--- Zaxzax.py
+++ Zaxzax.py
@@ -1,6 +1,6 @@
-print('Hello Franz')
+print('Hello Franz!')
 
-print('Hello TommyGun')
-print('Hello TommyGun')
+print('Hello TommyGun!')
+print('Hello TommyGun!')
 
-print('Thorsten Frings')
+print('Thorsten Frings!')
